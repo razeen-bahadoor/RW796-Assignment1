2c60dbebd4c456037c0c09183b97855ab2c32f7f
razeen <bahadoor30@gmail.com>
2021-09-05 17:03:39 +0200

    pre-demo test

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -43,7 +43,7 @@ public class StringUtils {
      * @return modified string
      */
     public StringUtils deleteCharAt(int index) {
-        if (false) {
+        if (index < 0 || index >= _internal.length()) {
             throw new StringIndexOutOfBoundsException("");
         } else {
             this._internal = this._internal.substring(0,index) +
